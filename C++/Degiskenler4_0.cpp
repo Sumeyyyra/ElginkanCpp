22378a01da96b495bdb1a056eab859078d2710d2
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-11-05 17:02:51 +0300

    Create Degiskenler4_0.cpp

---
+++ C++/Degiskenler4_0.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /* \n alt satira geçiş
+    \t tab kadar boşluk bırakır
+    \v dikey tab kaar boşluk borakır
+    \r imleci satır başına konumlandırır
+    \a uyarı amaçlı bir ses çıkarır
+    \b kendinden önceki karakteri silmek
+    \0 null karakter dizilerini string hale getirir.
+    \\ ekrana \ yazdırmak için kullanılır
+    \" ekkrana tırnak yazdırmak için kullanılır  
+    */ 
+   cout<<"\t*********************************"<<endl;
+   cout<<"\t* C++ Programlamaya Hoşgeldiniz*"<<endl;
+   cout<<"\t*********************************\n"<<endl;
+   return 0;
+
+}
