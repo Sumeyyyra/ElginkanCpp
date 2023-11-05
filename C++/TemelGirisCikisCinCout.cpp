17cccc8dca00abb974770bd03a089e22f382d6f1
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-11-05 16:49:54 +0300

    Create TemelGirisCikisCinCout.cpp

---
+++ C++/TemelGirisCikisCinCout.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //atama işlemi
+    int sayi,sayi2;
+
+    //kullanicidan değer  alma ve yazdırma
+    cout<<"Bir sayi girin:";
+    cin>>sayi;
+    cout<<"Ikinci sayiyi girin:";
+    cin>>sayi2;
+    cout<<"sayi1:"<<sayi<<endl<<"sayi2:"<<sayi2;
+}
