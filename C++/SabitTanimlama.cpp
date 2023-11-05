405c836cf0600dc08cf571162d196737021487e1
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-11-05 19:28:10 +0300

    Create SabitTanimlama.cpp

---
+++ C++/SabitTanimlama.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+const int yas =18;
+
+int main()
+{
+    //sabit tanımlaması yaparken const yapısını kullanıyoruz.
+    cout<<yas;
+    //yası bundan sonra sadece okuyabiliriz üzerinde değer değişikliği yapamayız.
+
+
+}
