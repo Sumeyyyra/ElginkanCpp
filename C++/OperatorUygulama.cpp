eb18bf9409647f0eab94558fe5fded76a78eee12
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-11-05 21:54:00 +0300

    Create OperatorUygulama.cpp

---
+++ C++/OperatorUygulama.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+
+int main()
+{
+//ORNEK : klavyeden girilen sayiların tek mi çift mi olduğunu bulan progami yaz
+/*
+int sayi;
+
+cin>>sayi;
+fmod(sayi,2) ==0? cout<<"cift":cout<<"tek";
+
+//ORNEK : vize-final 
+
+int sozlu1,sozlu2,vize,fnal;
+cin>>sozlu1>>sozlu2;
+cin>>vize>>fnal;
+
+float ort1=(sozlu1+sozlu2+vize)/3;
+cout<<ort1;
+cout<<"sonuc:"<<(ort1*0.4)+(fnal*0.6);
+*/
+
+//ORNEK3 : Vize finalde yüzdeleri geçme notunu verince min kaç alması gerektiğini veren kod.
+
+
+}
