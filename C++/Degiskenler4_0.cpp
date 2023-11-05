abfbf7e2db1aa5cc5b2d062bc29bdc29ffcbfceb
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-11-05 17:15:52 +0300

    Update Degiskenler.cpp

--- C++/Degiskenler4_0.cpp
+++ C++/Degiskenler4_0.cpp
@@ -14,6 +14,33 @@ int main()
     \\ ekrana \ yazdırmak için kullanılır
     \" ekkrana tırnak yazdırmak için kullanılır  
     */ 
+
+        /* Değişkenler bilgisayar hafızasında verileri depolayan ve isimleri olan programlamanın en temel elementidir.
+    Programlarımızda işlemlerimizi yaparken verileri kullanırız.
+    Mesela herhangi iki sayıyı toplarız veya iki tane karakter(string) karşılaştırırız.
+    Bu işlemler için kullandığımız verilerimizi değişkenler içinde tutarız.  
+    */ 
+ 
+    /*Değişkenleri tanımlarken isimlendirmemiz gerekir.
+    Ardından hazfızada oluşturduğumuz değişkene göre adres tutar
+    Değişken isimleri aslında bizim adrese ulaşmamızı sağlayan araçlardır.
+    C++ da bir değişkeni kullanmadan tanımlamamız gerekmektedir.
+    Değişkenlerimizde c++ da kullanılan  anahtar kelimeleri kullanamayız. long int case if...
+    */
+
+   /*Veri türleri program çalışırken RAM'de depolanır.
+   Hafızada kapladıkları alanı byte üzerinden ifade ederiz.
+   */
+
+  //<veri türü> <değiken adı>;
+
+  //int long short tam sayi değerleri için. int ve long 4 byte 
+  //short 2 byte yer turat .Genel hesaplama=> 2byte=16 bit => 2^16 tane sayi yazabilir
+    /* float 4 byte 
+        double 8 byte
+        bool 1 byte
+    */
+    
    cout<<"\t*********************************"<<endl;
    cout<<"\t* C++ Programlamaya Hoşgeldiniz*"<<endl;
    cout<<"\t*********************************\n"<<endl;
