9a46bcea176e237f2ba4be95d5e7b894c2b33820
Sümeyra USTA <117122279+Sumeyyyra@users.noreply.github.com>
2023-11-06 14:44:14 +0300

    Create SwitchCase7_7_.cpp

---
+++ C++/SwitchCase7_7_.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+//Switch-case
+
+//Ornek : Sistem kontrol
+/*
+cout<<"basinci girin: ";
+int basinc;
+cin>>basinc;
+
+switch (basinc)
+{
+case 8:
+    cout<<"Basinc yuksek;Sistemi kontorl et.";
+    break;
+
+case 7:
+    cout<<"Sicakligi dusur ; Surekli kontorl et.";
+    break;
+
+case 6:
+    cout<<"Dikkat ; Her 5 dakikada bir kontol et.";
+    break;
+default:
+    cout<<"Calisma kosullari normal.";
+    break;
+}*/
+
+//ornek : dort islem
+/*
+int a,b;
+cin>>a;
+cin>>b;
+
+char islem;
+cout<<"islem sec";
+cin>>islem;
+
+switch (islem)
+{
+case '+':
+    cout<<a+b;
+    break;
+case '-':
+    cout<<a-b;
+    break;
+case '*':
+    cout<<a*b;
+    break;
+case '/':
+    cout<<(float)a/b;
+    break;
+
+default:
+    break;
+}*/
+
+
+//Ornek : yazılışı verilen sayiyi okunuşa çevirme 3 bas
+
+}
